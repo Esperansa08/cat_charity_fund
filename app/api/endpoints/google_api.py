6262c24069b1d5d926d8082154ff4122ef28430a
Анастасия Савельева <Visteria09@yandex.ru>
2023-09-21 23:23:29 +0700

    правка 2

--- app/api/endpoints/google_api.py
+++ app/api/endpoints/google_api.py
@@ -17,6 +17,7 @@ router = APIRouter()
 
 @router.post(
     '/',
+    response_model=str,
     dependencies=[Depends(current_superuser)],)
 async def get_report(
         session: AsyncSession = Depends(get_async_session),
